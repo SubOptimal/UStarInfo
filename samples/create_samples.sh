9c2aaff528045110c0427e2ec83dc44342dee3d2
Frank Dietrich <bits_n_bytes@gmx.de>
2015-12-12 23:22:22 +0100

    add script to generate some tar file examples

---
+++ samples/create_samples.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# script to generate valid and some invalid tar file samples
+
+# create a valid tar file
+tar cf valid.tar ../pom.xml
+
+# create a tar file with an invalid mtime field
+# (the checksum is not recomputed and therefore is invalid too)
+cp valid.tar invalid_mtime.tar
+printf 12345678901 > broken_mtime.field
+printf 8 > broken_mtime.field
+dd if=broken_mtime.field of=invalid_mtime.tar bs=1 seek=136 conv=notrunc
+rm broken_mtime.field
+
+# create a tar file with an invalid checksum field
+cp valid.tar invalid_checksum.tar
+printf 424242 > broken_checksum.field
+dd if=broken_checksum.field of=invalid_checksum.tar bs=1 seek=148 conv=notrunc
+rm broken_checksum.field
